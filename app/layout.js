1ba7cbacac3c4b0e0e464fb185f4da12cd4aa8dd
brett-the-jet21 <brettdeavila@gmail.com>
2026-01-18 13:51:52 -0800

    Add Google AdSense script to layoutAdd Google AdSense global script to root layout

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
+      <head>
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
     </html>
   );
 }
